ca7dcc596e888b5b1f5fc003349e84cd236f145d
demidovv <TaJ34$xh>
2020-05-04 11:51:55 +0300

    Корректировка Multimax

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -1,9 +1,11 @@
 package ru.job4j.condition;
 
 public class MultiMax {
-    @SuppressWarnings("checkstyle:NoWhitespaceBefore")
+
     public int max(int first, int second, int third) {
-        int result = first > second && first > third ? first : second > third ? second : third;
+        int result = first >= second ? first : second;
+        result = result >= third ? result : third;
+
         return result;
     }
 }
