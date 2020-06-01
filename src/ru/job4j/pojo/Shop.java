20c8da60271d388327947d8af2953b90458c98ab
demidovv <demidov011@gmail.com>
2020-06-01 08:24:38 +0300

    3. Удаление моделей из массива. [#279872]

---
+++ src/ru/job4j/pojo/Shop.java
@@ -0,0 +1,95 @@
+package ru.job4j.pojo;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Shop {
+    public static Product[] delete(Product[] products, int index) {
+        for (int i = index; i < products.length - 1; i++) {
+            products[i] = products[i + 1];
+        }
+        products[products.length - 1] = null;
+        return products;
+    }
+
+    public static void main(String[] args) {
+        Product products[] = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            //проверяем, что объект не равен null. тк у нас массив не заполнен целиком.
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Удаляем значение из ячейки с индексом 1");
+        //удаляем значение из ячейки с индексом 1
+        products[1] = null;
+
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            //проверяем, что объект не равен null. тк у нас массив не заполнен целиком.
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Записываем  в ячейку с индексом 1 значение ячейки с индексом 2 и удаляем значение из ячейки с индексом 2");
+        //записываем в ячейку с индексом 1 значение ячейки с индексом 2.
+        products[1] = products[2];
+        //удаляем значение из ячейки с индексом 2.
+        products[2] = null;
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            //проверяем, что объект не равен null. так как  у нас массив не заполнен целиком.
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        //Создаем второй список продуктов и удаляем оттуда элемент методом delete
+        Product products2[] = new Product[5];
+        products2[0] = new Product("Milk", 10);
+        products2[1] = new Product("Bread", 4);
+        products2[2] = new Product("Egg", 19);
+        products2[3] = new Product("Water", 6);
+        products2[4] = new Product("Tomat", 2);
+
+        System.out.println("Новый список продуктов");
+        for (int i = 0; i < products2.length; i++) {
+            Product product = products2[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Удаляем элемент из списка продуктов:");
+
+        delete(products2, 1);
+
+        for (int i = 0; i < products2.length; i++) {
+            Product product = products2[i];
+            //проверяем, что объект не равен null. так как  у нас массив не заполнен целиком.
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+    }
+}
