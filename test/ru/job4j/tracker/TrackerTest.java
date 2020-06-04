50ca347eed4f3fc21ff8acb8f2a38537324bd9a2
demidovv <demidov011@gmail.com>
2020-06-04 07:39:30 +0300

    8. Что такое валидация? [#279875]

--- test/ru/job4j/tracker/TrackerTest.java
+++ test/ru/job4j/tracker/TrackerTest.java
@@ -60,7 +60,7 @@ public class TrackerTest {
         Item bug = new Item("Bug");
         tracker.add(bug);
         String id = bug.getId();
-        Item bugWithDesc = new Item("Bug with description");;
+        Item bugWithDesc = new Item("Bug with description");
         tracker.replace(id, bugWithDesc);
         assertThat(tracker.findById(id).getName(), is("Bug with description"));
     }
