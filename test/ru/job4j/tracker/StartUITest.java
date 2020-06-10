8096262b6d7c06decb62364cf50ac3a1ff82fa02
demidovv <demidov011@gmail.com>
2020-06-10 08:11:46 +0300

    5.1. Тестирование. Подготовка данных. [#279817]

--- test/ru/job4j/tracker/StartUITest.java
+++ test/ru/job4j/tracker/StartUITest.java
@@ -22,5 +22,32 @@ public class StartUITest {
             assertThat(created[i].getName(), is(answers[i]));
         }
     }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                item.getId(), // id сохраненной заявки в объект tracker.
+                "replaced item"
+        };
+        StartUI.replaceItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                item.getId(), // id сохраненной заявки в объект tracker.
+        };
+        StartUI.deteleItem(new StubInput(answers), tracker);
+        Item excepted = null;
+        assertThat(tracker.findById(item.getId()), is(excepted));
+    }
 }
 
