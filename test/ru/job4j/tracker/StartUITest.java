5720a8ae8ccbd897bfe88a0d80d55cae8a23835f
demidovv <demidov011@gmail.com>
2020-06-10 07:31:45 +0300

    5. Input и полиморфизм. [#279812] - корректировка

--- test/ru/job4j/tracker/StartUITest.java
+++ test/ru/job4j/tracker/StartUITest.java
@@ -19,8 +19,8 @@ public class StartUITest {
 
         for (int i = 0; i < numbers; i++) {
             Item created = tracker.findAll()[i];
-            Item expected = new Item(answers[i]);
-            assertThat(created.getName(), is(expected.getName()));
+            assertThat(created.getName(), is(answers[i]));
         }
     }
 }
+
